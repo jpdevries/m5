5f0af71c892121b9e88828a3033befbb189d5b2f
JP de Vries <mail@devries.jp>
2017-01-08 16:15:21 -0800

    adding postcss-cssnext
    This reverts commit a089a02703f76542913ceef4a48ae27a5d497d82.

--- gruntfile.js
+++ gruntfile.js
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
         processors: [
           require('pixrem')(), // add fallbacks for rem units
           require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-          require("postcss-color-function")(),
+          require("postcss-cssnext")()
           //require("css-mqpacker")()
           //require('cssnano')() // minify the result
         ]
